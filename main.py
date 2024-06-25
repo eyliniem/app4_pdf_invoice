821b6ed215d72899b6632f2230348c64289583da
eylinem <eno@voima-technical.com>
2024-06-24 19:13:26 -0700

    add headers

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
     
     pdf = FPDF(orientation='P', unit="in", format='letter')
     pdf.add_page()
@@ -18,10 +17,27 @@ for filepath in filepaths:
     pdf.cell(w=2, h=.3, txt=f"Invoice Number: {invoice_no}", ln=1)
     
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=2, h=.3, txt=f"Date: {inv_date}")
+    pdf.cell(w=2, h=.3, txt=f"Date: {inv_date}", ln=2)
  
-    
-    
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    header_columns = df.columns
+    header_columns = [item.replace("_", " ").title() for item in header_columns]
+    pdf.set_font(family="Times", size=9, style="B")
+    pdf.set_text_color(0,0,0)
+    pdf.cell(w=1, h=.5, txt=header_columns[0],border=1)
+    pdf.cell(w=2, h=.5, txt=header_columns[1], border=1)
+    pdf.cell(w=1.2, h=.5, txt=header_columns[2], border=1, align='R')
+    pdf.cell(w=1, h=.5, txt=header_columns[3], border=1, align='R')
+    pdf.cell(w=1, h=.5, txt=header_columns[4], border=1, ln=1, align='R')
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(200,0,50)
+        pdf.cell(w=1, h=.5, txt=str(row["product_id"]),border=1)
+        pdf.cell(w=2, h=.5, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=1.2, h=.5, txt=str(row["amount_purchased"]), border=1, align='R')
+        pdf.cell(w=1, h=.5, txt=str(row["price_per_unit"]), border=1, align='R')
+        pdf.cell(w=1, h=.5, txt=str(row["total_price"]), border=1, ln=1, align='R')
     
     
     pdf.output(f"pdf_output/Inv{invoice_no}.pdf")
